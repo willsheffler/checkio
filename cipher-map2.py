ad70bf036ed7cba3cf079d23a8dc1029f520e9f2
Will Sheffler <willsheffler@gmail.com>
2017-02-21 23:16:04 -0800

    cipher-map2

---
+++ cipher-map2.py
@@ -0,0 +1,26 @@
+def recall_password(cipher_grille, ciphered_password):
+    return ""
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for
+    # auto-testing
+    assert recall_password(
+        ('X...',
+         '..X.',
+         'X..X',
+         '....'),
+        ('itdf',
+         'gdce',
+         'aton',
+         'qrdi')) == 'icantforgetiddqd', 'First example'
+
+    assert recall_password(
+        ('....',
+         'X..X',
+         '.X..',
+         '...X'),
+        ('xhwc',
+         'rsqx',
+         'xqzz',
+         'fyzr')) == 'rxqrwsfzxqxzhczy', 'Second example'
